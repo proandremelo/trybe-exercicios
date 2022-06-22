f18e64caa22225290ccc86efca6df1b210ac68f7
André Melo <proandremelo@gmail.com>
2022-06-22 17:32:11 -0300

    Item 3

---
+++ fundamentos/b4-introjavascript/d1-primeirospassos/item3.js
@@ -0,0 +1,11 @@
+const a = 22;
+const b = 17;
+const c = 16;
+
+if( a > b && a > c){
+    console.log('a: ' + a);
+}else if( b > a && b > c){
+    console.log('b: ' + b);
+}else{
+    console.log('c: ' + c);
+}
