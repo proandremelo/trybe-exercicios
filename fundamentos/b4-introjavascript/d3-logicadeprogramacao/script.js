9d13032ba76967a89b80f84867d65d3bac3d88a8
André Melo <proandremelo@gmail.com>
2022-06-23 20:24:21 -0300

    Item 4

--- fundamentos/b4-introjavascript/d3-logicadeprogramacao/script.js
+++ fundamentos/b4-introjavascript/d3-logicadeprogramacao/script.js
@@ -8,7 +8,6 @@
 
 // console.log(resultado);
 
-
 //////////////////// 2 ////////////////////
 
 // let word = 'tryber';
@@ -22,38 +21,56 @@
 
 //////////////////// 3 ////////////////////
 
-            ///// MAIOR /////
-            
-            // let array = ['java', 'javascript', 'python', 'html', 'css'];
-            // let maiorIndice = 0;
-            // let maiorTamanho = 0;
-            
-            // for (let index = 0; index < array.length; index++) {
-            //     if (array[index].length > maiorTamanho ) {
-            //         maiorIndice = index;
-            //         maiorTamanho = array[index].length;
-            //     }
-                
-            // }
+///// MAIOR /////
 
-            // console.log(array[maiorIndice]);
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorIndice = 0;
+// let maiorTamanho = 0;
 
-            ///// MENOR /////
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index].length > maiorTamanho ) {
+//         maiorIndice = index;
+//         maiorTamanho = array[index].length;
+//     }
 
-            let array = ['java', 'javascript', 'python', 'html', 'css'];
-            let menorIndice = 0;
-            let menorTamanho = 999999999999999999;
-            
-            for (let index = 0; index < array.length; index++) {
-                if (array[index].length < menorTamanho ) {
-                    menorIndice = index;
-                    menorTamanho = array[index].length;
-                }
-                
-            }
+// }
 
-            console.log(array[menorIndice]);
+// console.log(array[maiorIndice]);
+
+///// MENOR /////
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let menorIndice = 0;
+// let menorTamanho = 999999999999999999;
+
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index].length < menorTamanho ) {
+//         menorIndice = index;
+//         menorTamanho = array[index].length;
+//     }
+
+// }
+
+// console.log(array[menorIndice]);
 
 //////////////////// 4 ////////////////////
 
+let numerosPrimos = [];
 
+for (let i = 2; i < 51; i++) {
+  if (i === 2) {
+    numerosPrimos.push(2);
+  }
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      break;
+    }
+
+    if (j === i - 1) {
+      numerosPrimos.push(i);
+    }
+  }
+}
+
+console.log(numerosPrimos);
