af0bd0105f613c3ea0217a25cc532765f2767e90
André Melo <proandremelo@gmail.com>
2022-06-23 20:04:36 -0300

    Item 3

--- fundamentos/b4-introjavascript/d3-logicadeprogramacao/script.js
+++ fundamentos/b4-introjavascript/d3-logicadeprogramacao/script.js
@@ -11,18 +11,48 @@
 
 //////////////////// 2 ////////////////////
 
-let word = 'tryber';
-let invertedWord = [];
+// let word = 'tryber';
+// let invertedWord = [];
 
-for (const letter of word) {
-    invertedWord.unshift(letter);
-}
+// for (const letter of word) {
+//     invertedWord.unshift(letter);
+// }
 
-console.log(invertedWord.join(''));
+// console.log(invertedWord.join(''));
 
 //////////////////// 3 ////////////////////
 
+            ///// MAIOR /////
+            
+            // let array = ['java', 'javascript', 'python', 'html', 'css'];
+            // let maiorIndice = 0;
+            // let maiorTamanho = 0;
+            
+            // for (let index = 0; index < array.length; index++) {
+            //     if (array[index].length > maiorTamanho ) {
+            //         maiorIndice = index;
+            //         maiorTamanho = array[index].length;
+            //     }
+                
+            // }
 
+            // console.log(array[maiorIndice]);
+
+            ///// MENOR /////
+
+            let array = ['java', 'javascript', 'python', 'html', 'css'];
+            let menorIndice = 0;
+            let menorTamanho = 999999999999999999;
+            
+            for (let index = 0; index < array.length; index++) {
+                if (array[index].length < menorTamanho ) {
+                    menorIndice = index;
+                    menorTamanho = array[index].length;
+                }
+                
+            }
+
+            console.log(array[menorIndice]);
 
 //////////////////// 4 ////////////////////
 
