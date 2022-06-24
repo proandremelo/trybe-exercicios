1027bb422921b4999ece96c1eabd8d823f210c03
André Melo <proandremelo@gmail.com>
2022-06-24 17:37:22 -0300

    Item 2

--- fundamentos/b4-introjavascript/d4-objetosefuncoes/parte2.js
+++ fundamentos/b4-introjavascript/d4-objetosefuncoes/parte2.js
@@ -1,19 +1,25 @@
 //////////////// 1 //////////////
 
-function verificaPalindrome(palavra) {
-  let tamanhoPalavra = palavra.length;
-  let metade1 = palavra.slice(0, Math.round(tamanhoPalavra / 2));
-  let metade2 = palavra.slice(Math.round(tamanhoPalavra / 2) - 1).split('').reverse().join('');
-  if (metade1 === metade2) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function verificaPalindrome(palavra) {
+//   let tamanhoPalavra = palavra.length;
+//   let metade1 = palavra.slice(0, Math.round(tamanhoPalavra / 2));
+//   let metade2 = palavra.slice(Math.round(tamanhoPalavra / 2) - 1).split('').reverse().join('');
+//   if (metade1 === metade2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-console.log(verificaPalindrome('arara'));
+// console.log(verificaPalindrome('arara'));
 
 //////////////// 2 //////////////
+
+function indexOfMaxValue(array){
+    return array.indexOf(Math.max(...array));
+}
+
+console.log(indexOfMaxValue([2, 3, 6, 47, 10, 1]));
 
 //////////////// 3 //////////////
 
