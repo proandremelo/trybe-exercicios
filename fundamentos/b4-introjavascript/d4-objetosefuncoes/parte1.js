142a0e226a4daca80d87544ef32af12aa9c2e4f0
André Melo <proandremelo@gmail.com>
2022-06-24 16:08:44 -0300

    Item 6

--- fundamentos/b4-introjavascript/d4-objetosefuncoes/parte1.js
+++ fundamentos/b4-introjavascript/d4-objetosefuncoes/parte1.js
@@ -27,22 +27,44 @@ let info = {
 
 ///////////////// 5 //////////////////
 
-let info2 = {
-  personagem: "Tio Patinhas",
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: "O último MacPatinhas",
-  recorrente: "Sim",
-};
+// let info2 = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim",
+// };
 
-for (const key in info) {
-  if (key === "recorrente" && info[key] === info2[key]) {
-    console.log("Ambos recorrentes");
-  } else {
-    console.log(info[key] + " e " + info2[key]);
-  }
-}
+// for (const key in info) {
+//   if (key === "recorrente" && info[key] === info2[key]) {
+//     console.log("Ambos recorrentes");
+//   } else {
+//     console.log(info[key] + " e " + info2[key]);
+//   }
+// }
 
 ///////////////// 6 //////////////////
+
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
+
+console.log(
+  "O livro favorito de " +
+    leitor.nome +
+    " " +
+    leitor.sobrenome +
+    " se chama " +
+    leitor.livrosFavoritos[0].titulo
+);
 
 ///////////////// 7 //////////////////
 
