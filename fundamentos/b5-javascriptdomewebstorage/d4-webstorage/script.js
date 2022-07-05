c90aaca759c5d6e19b9c25112775eb532e8a0bf4
André Melo <proandremelo@gmail.com>
2022-07-05 20:08:30 -0300

    Exercício Resolvido

--- fundamentos/b5-javascriptdomewebstorage/d4-webstorage/script.js
+++ fundamentos/b5-javascriptdomewebstorage/d4-webstorage/script.js
@@ -1,21 +1,43 @@
-let corDeFundo = document.querySelector("#cor-de-fundo");
-let corDoTexto = document.querySelector("#cor-do-texto");
-let tamanhoFonte = document.querySelector("#tamanho-fonte");
-let tipoFonte = document.querySelector("#tipo-fonte");
-let espacamento = document.querySelector("#espacamento");
 let aplicar = document.querySelector("#aplicar");
 let salvar = document.querySelector("#salvar");
+let body = document.body;
+let main = document.querySelector("main");
 
 aplicar.addEventListener("click", apply);
 salvar.addEventListener("click", save);
 
-function apply(storage){
-    if (storage) {
-        
-    }
+function apply() {
+  let corDeFundo = document.querySelector("#cor-de-fundo input").value;
+  let corDoTexto = document.querySelector("#cor-do-texto input").value;
+  let tamanhoFonte = document.querySelector("#tamanho-fonte input").value;
+  let tipoFonte = document.querySelector("#tipo-fonte input").value;
+  let espacamento = document.querySelector("#espacamento input").value;
+
+  body.style.backgroundColor = corDeFundo;
+  main.style.color = corDoTexto;
+  main.style.fontSize = tamanhoFonte;
+  main.style.fontFamily = tipoFonte;
+  main.style.lineHeight = espacamento;
 }
 
-function save(){
-
+function save() {
+  let settings = {
+    corDeFundo: document.querySelector("#cor-de-fundo input").value,
+    corDoTexto: document.querySelector("#cor-do-texto input").value,
+    tamanhoFonte: document.querySelector("#tamanho-fonte input").value,
+    tipoFonte: document.querySelector("#tipo-fonte input").value,
+    espacamento: document.querySelector("#espacamento input").value,
+  };
+  localStorage.setItem("settings", JSON.stringify(settings));
 }
 
+window.onload = function () {
+  if (localStorage.getItem("settings")) {
+    let settings = JSON.parse(localStorage.getItem("settings"));
+    body.style.backgroundColor = settings.corDeFundo;
+    main.style.color = settings.corDoTexto;
+    main.style.fontSize = settings.tamanhoFonte;
+    main.style.fontFamily = settings.tipoFonte;
+    main.style.lineHeight = settings.espacamento;
+  }
+};
