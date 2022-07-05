cfaa0bc3656c2f3a2c18f18cbaba57821c3fa097
André Melo <proandremelo@gmail.com>
2022-07-05 18:00:21 -0300

    Primeiros Passos - html, css e js criados e iniciados

---
+++ fundamentos/b5-javascriptdomewebstorage/d4-webstorage/script.js
@@ -0,0 +1,21 @@
+let corDeFundo = document.querySelector("#cor-de-fundo");
+let corDoTexto = document.querySelector("#cor-do-texto");
+let tamanhoFonte = document.querySelector("#tamanho-fonte");
+let tipoFonte = document.querySelector("#tipo-fonte");
+let espacamento = document.querySelector("#espacamento");
+let aplicar = document.querySelector("#aplicar");
+let salvar = document.querySelector("#salvar");
+
+aplicar.addEventListener("click", apply);
+salvar.addEventListener("click", save);
+
+function apply(storage){
+    if (storage) {
+        
+    }
+}
+
+function save(){
+
+}
+
